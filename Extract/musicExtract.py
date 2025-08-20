7132c10cfba2cd9ada91c2b492d1b058da13800c
Isabella ALejandra Hernandez Angulo <iahernandeza@academia.usbbog.edu.co>
2025-08-20 16:17:52 +0000

    Created things: class for de cvs file also link for dataframe

---
+++ Extract/musicExtract.py
@@ -0,0 +1,16 @@
+import requests
+import pandas as pd
+import numpy as np
+
+class MusicExtract:
+    
+    def __init__(self, csv_path):
+        self.csv = csv_path
+
+    def queries(self):
+        data = pd.read_csv(self.csv)
+        return data
+
+    def response(self):
+        data = self.queries()
+        return data.head(5)
